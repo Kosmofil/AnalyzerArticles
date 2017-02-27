6fd1c3375b0f58ee8e059b8f0a4662a135852a3d
KONSTANTIN <konstantin.turchenko@gmail.com>
2017-02-28 00:19:54 +0600

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,37 +21,45 @@ public class Main {
     }
 
     public static void get() throws IOException {
-        // TODO: 22.02.17 разобраться заменить forEach
-        // TODO: 22.02.17 переделать метод по совету NS (без интернета!)
 
         List<Document> documents = new ArrayList<>();
         final String url = "https://habrahabr.ru/top/alltime/";
-                rangeClosed(0,3).forEach(i ->{
+        rangeClosed(0, 1).forEach(i -> {
+            try {
+                documents.add(Jsoup.connect(url + "page" + i).userAgent("Mozilla").get());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Collection<String> text = documents.stream()//получили весь текст со статей
+                .map(i -> i.select(".post__title_link"))//+
+                .flatMap(w -> w.stream()
+                        .map(q -> q.absUrl("href")))//получили список ссылок на каждую статью +
+                .map(t -> {
                     try {
-                        documents.add(Jsoup.connect(url + "page" + i).userAgent("Mozilla").get());
+                        return Jsoup.connect(t).get().select(".post_show");//без коннекта, разобраться с парсером
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                });
+                    return null;//убрать нул
+                })
+                .map(z -> getNewText(z, "div[class=post__body post__body_full]"))
+                // TODO: 28.02.17 нужен метод красиво обрабатывающий текст
+                .collect(Collectors.toList());
 
-                Collection<String> row = documents.stream()
-                        .map(i->i.select(".post__title_link"))
-                        .map(w->w.outerHtml())//далее проход по ссылкам и обработка текста.
-                        .collect(Collectors.toList());
+        // TODO: 28.02.17 сделать метод выводящий статистику
 
     }
 
 
-
-    private static List<Elements> getPost(String cssQuery){
-
+    private static List<Elements> getPost(String cssQuery) {
         List<Elements> list = new ArrayList<>();
-        list.stream().map(i->i.select(cssQuery));
+        list.stream().map(i -> i.select(cssQuery));
         return list;
     }
 
     private static String getNewText(Elements element, String cssQuery) {//получаем текст
-
         Elements result = element.select(cssQuery);
         if (result != null) {
             return result.text();
